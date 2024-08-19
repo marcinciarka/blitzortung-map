a3443be8d048a5ab995eb77b7e1ad3b083ad732e
Marcin Ciarka <marcin@oazoapps.com>
2024-08-19 15:20:30 +0200

    feat: Update npm dependencies for @types/lodash and lodash

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -8,6 +8,8 @@ import Image from "next/image";
 
 import strikeIcon from "../app/strike.png";
 import dayjs from "dayjs";
+import { memoize } from "lodash";
+import timeAgo from "dayjs/plugin/relativeTime";
 import {
   NEXT_PUBLIC_API_URL_WEBSOCKET,
   NEXT_PUBLIC_HOME_LAT,
@@ -16,42 +18,76 @@ import {
 } from "@/constants";
 import { useEffect, useState } from "react";
 
+dayjs.extend(timeAgo, {
+  thresholds: [
+    { l: "s", r: 1 },
+    { l: "m", r: 1 },
+    { l: "mm", r: 600, d: "minute" },
+  ],
+});
+
+const showTimeRange = 60; // seconds
+
 const mapCenter = [NEXT_PUBLIC_HOME_LAT, NEXT_PUBLIC_HOME_LONG] as [
   number,
   number
 ];
 
-const Strike = ({
-  strike,
-  timeRange,
-}: {
-  strike: StrikeEntity;
-  timeRange: number;
-}) => {
-  const now = dayjs();
-  const relevancy =
-    (1 + dayjs(strike.last_changed).diff(now, "minute") / 10) * timeRange;
+const Strike = memoize(
+  ({ strike, timeRange }: { strike: StrikeEntity; timeRange: number }) => {
+    const now = dayjs();
+    const timeRangeFinal = (timeRange / 100) * showTimeRange;
+    const relevant = dayjs(now).isBefore(
+      dayjs(strike.last_changed).add(timeRangeFinal, "minute")
+    );
 
-  return (
-    <Marker
-      key={strike.entity_id}
-      position={[strike.attributes.latitude, strike.attributes.longitude]}
-    >
-      <Image
-        src={strikeIcon}
-        alt=""
-        height={relevancy !== 0 ? 12 * relevancy : 1}
-        width={relevancy !== 0 ? 12 * relevancy : 1}
-        style={{
-          opacity: 1 * relevancy,
-        }}
-        className={`absolute markerAppear z-20 h-[${12 * relevancy}px] w-[${
-          12 * relevancy
-        }px] rounded-full`}
-      />
-    </Marker>
-  );
-};
+    const hoverData = (
+      <div className="group-hover:opacity-100 pointer-events-none opacity-0 absolute z-25 top-[-40px] h-[30px] left-[-41px] w-[100px] text-center bg-slate-900/50 text-white backdrop-blur-lg rounded-lg p-2">
+        {dayjs(strike.last_changed).fromNow()}
+      </div>
+    );
+
+    return relevant ? (
+      <Marker
+        key={strike.entity_id}
+        position={[strike.attributes.latitude, strike.attributes.longitude]}
+      >
+        <div className="group">
+          <Image
+            src={strikeIcon}
+            title={dayjs(strike.last_changed).fromNow()}
+            alt={dayjs(strike.last_changed).fromNow()}
+            height={18}
+            width={18}
+            className="absolute markerAppear z-20 h-[18px] w-[18px] rounded-full"
+          />
+          {hoverData}
+        </div>
+      </Marker>
+    ) : (
+      <Marker
+        key={strike.entity_id}
+        position={[strike.attributes.latitude, strike.attributes.longitude]}
+      >
+        <div className="group">
+          <Image
+            src={strikeIcon}
+            title={dayjs(strike.last_changed).fromNow()}
+            alt={dayjs(strike.last_changed).fromNow()}
+            height={8}
+            width={8}
+            style={{
+              opacity: 1,
+              filter: "saturate(0%)",
+            }}
+            className="absolute markerAppear z-20 h-[8px] w-[8px] rounded-full"
+          />
+          {hoverData}
+        </div>
+      </Marker>
+    );
+  }
+);
 
 export const Map = ({
   baseStrikesData,
@@ -62,7 +98,7 @@ export const Map = ({
 }) => {
   const [connected, setConnected] = useState<boolean>(false);
   const [strikes, setStrikes] = useState<StrikeEntity[]>(baseStrikesData);
-  const [timeRange, setTimeRange] = useState<number>(1);
+  const [timeRange, setTimeRange] = useState<number>(100);
 
   useEffect(() => {
     const ws = new WebSocket(NEXT_PUBLIC_API_URL_WEBSOCKET as string);
@@ -111,21 +147,24 @@ export const Map = ({
         <p>Lightning count: {strikes.length}</p>
         <p>Backend: {connected ? "Connected" : "Not connected"}</p>
         {/* slider */}
-        <label htmlFor="range">Time range:</label>
+        <label htmlFor="range">Time range (% of 1h):</label>
         <input
           type="range"
           min="0"
-          max="10"
+          max="100"
           step="1"
-          value={timeRange * 10}
+          value={timeRange}
           onChange={(e) => {
-            setTimeRange(parseInt(e.target.value) / 10);
+            setTimeRange(parseFloat(e.target.value));
           }}
         />
+        <p>
+          Showing last: {Math.ceil((timeRange / 100) * showTimeRange)} minutes
+        </p>
       </div>
       <MapContainer
         center={mapCenter}
-        zoom={7}
+        zoom={12}
         scrollWheelZoom={true}
         style={{ height: "100vh", width: "100vw", zIndex: 0 }}
         fadeAnimation={false}
